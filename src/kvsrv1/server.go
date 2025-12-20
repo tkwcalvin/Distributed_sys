ae8392efeb4386585b3cfc3d03090abf4b3e9745
TANG Kewei <haona3043@gmail.com>
2025-12-21 00:50:36 +0800

    reliable input

--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -2,7 +2,12 @@ package kvsrv
 
 import (
 	"log"
+	"net"
+	"net/http"
+	"os"
 	"sync"
+
+	netrpc "net/rpc"
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labrpc"
@@ -25,52 +30,71 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 
 type KVServer struct {
 	mu sync.Mutex
-	db map[string]Entry
+	db map[string]*Entry
 	// Your definitions here.
 }
 
 func MakeKVServer() *KVServer {
 	kv := &KVServer{}
 	// Your code here.
+	// start a thread that listens for RPCs from worker.go
+	netrpc.RegisterName("KVServer", kv)
+	netrpc.HandleHTTP()
+	//l, e := net.Listen("tcp", ":1234")
+	sockname := rpc.SocketName()
+	os.Remove(sockname)
+	l, e := net.Listen("unix", sockname)
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	go http.Serve(l, nil)
+
+	kv.db = make(map[string]*Entry)
 	return kv
 }
 
 // Get returns the value and version for args.Key, if args.Key
 // exists. Otherwise, Get returns ErrNoKey.
-func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
+func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) error {
 	// Your code here.
 	kv.mu.Lock()
 	defer kv.mu.Unlock()
 	entry, ok := kv.db[args.Key]
 	if !ok {
 		reply.Err = rpc.ErrNoKey
-		return
+		return nil
 	}
 	reply.Value = entry.Value
 	reply.Version = entry.Version
+	reply.Err = rpc.OK
+	return nil
 }
 
 // Update the value for a key if args.Version matches the version of
 // the key on the server. If versions don't match, return ErrVersion.
 // If the key doesn't exist, Put installs the value if the
 // args.Version is 0, and returns ErrNoKey otherwise.
-func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
+func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) error {
 	// Your code here.
 	kv.mu.Lock()
 	defer kv.mu.Unlock()
 	entry, ok := kv.db[args.Key]
 	if !ok {
-		reply.Err = rpc.ErrNoKey
-		return
+		if args.Version != 0 {
+			reply.Err = rpc.ErrNoKey
+			return nil
+		}
+		reply.Err = rpc.OK
+		kv.db[args.Key] = &Entry{Value: args.Value, Version: args.Version + 1}
+		return nil
 	}
 	if args.Version != entry.Version {
 		reply.Err = rpc.ErrVersion
-		return
+		return nil
 	}
-	entry.Value = args.Value
-	entry.Version = args.Version
 	reply.Err = rpc.OK
-	kv.db[args.Key] = entry
+	kv.db[args.Key] = &Entry{Value: args.Value, Version: args.Version + 1}
+	return nil
 }
 
 // You can ignore Kill() for this lab
